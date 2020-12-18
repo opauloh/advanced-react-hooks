fdfb42f90fa00972e15ca0cbe5439d36dcd7f126
Paulo Henrique <paulo.scape@gmail.com>
2020-12-18 10:16:05 -0300

    change to traditional reducer with dispatch

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,18 +3,28 @@
 
 import * as React from 'react'
 
-const countReducer = (state, action) => ({
-  ...state,
-  ...(typeof action === 'function' ? action(state) : action),
-})
+// const countReducer = (state, action) => ({
+//   ...state,
+//   ...(typeof action === 'function' ? action(state) : action),
+// })
+const countReducer = (state, action) => {
+  if (action.type === 'INCREMENT')
+    return {...state, count: state.count + action.step}
+
+  return state
+}
 
 function Counter({initialCount = 0, step = 1}) {
-  const [{count}, setState] = React.useReducer(countReducer, {
+  // const [{count}, setState] = React.useReducer(countReducer, {
+  //   count: initialCount,
+  // })
+  const [{count}, dispatch] = React.useReducer(countReducer, {
     count: initialCount,
   })
 
-  const increment = () =>
-    setState(currentState => ({count: currentState.count + step}))
+  // const increment = () =>
+  // setState(currentState => ({count: currentState.count + step}))
+  const increment = () => dispatch({type: 'INCREMENT', step})
   return <button onClick={increment}>{count}</button>
 }
 
