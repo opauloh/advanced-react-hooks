a52ca4d7116e971790c013a3f258b3e8a8b8725c
Paulo Henrique <paulo.scape@gmail.com>
2020-12-18 10:09:53 -0300

    supporting both api (with function or object)

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,9 +3,10 @@
 
 import * as React from 'react'
 
-const countReducer = (state, action) => {
-  return action(state)
-}
+const countReducer = (state, action) => ({
+  ...state,
+  ...(typeof action === 'function' ? action(state) : action),
+})
 
 function Counter({initialCount = 0, step = 1}) {
   const [{count}, setState] = React.useReducer(countReducer, {
