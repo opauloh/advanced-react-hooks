8a48807c890e53f73bfa859aeefc0cc6378da61e
Paulo Henrique <paulo.scape@gmail.com>
2020-12-18 09:57:45 -0300

    useReducer accept step as props

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,25 +3,19 @@
 
 import * as React from 'react'
 
-function countReducer(state, newState) {
-  return newState
+function countReducer(state, step) {
+  return state + step
 }
 
 function Counter({initialCount = 0, step = 1}) {
-  // 🐨 replace React.useState with React.useReducer.
-  // 💰 React.useReducer(countReducer, initialCount)
-  const [count, setCount] = React.useReducer(countReducer, initialCount)
+  const [count, changeCounter] = React.useReducer(countReducer, initialCount)
 
-  // 💰 you can write the countReducer function so you don't have to make any
-  // changes to the next two lines of code! Remember:
-  // The 1st argument is called "state" - the current value of count
-  // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setCount(count + step)
+  const increment = () => changeCounter(step)
   return <button onClick={increment}>{count}</button>
 }
 
 function App() {
-  return <Counter />
+  return <Counter step={3} />
 }
 
 export default App
